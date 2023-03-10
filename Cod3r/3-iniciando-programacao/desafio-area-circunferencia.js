492398420313960b7f2627fb3ac568f0dfa8d848
Débora <deboraparecidamas@gmail.com>
2023-03-10 01:45:50 -0300

    desafios sec 3

---
+++ Cod3r/3-iniciando-programacao/desafio-area-circunferencia.js
@@ -0,0 +1,4 @@
+const pi = 3.14
+const raio = 10
+const areaCircunferencia = pi * raio * raio
+console.log('O valor da área é: ' + areaCircunferencia)
---
+++ Cod3r/3-iniciando-programacao/desafio-troca-valores.js
@@ -0,0 +1,9 @@
+let a = 7
+let b = 94
+
+let valorTemporario = a
+a = b
+b = valorTemporario
+
+console.log(a)
+console.log(b)
