e46f83e7686643dc13915531af699484a8f428fa
Débora <deboraparecidamas@gmail.com>
2023-03-13 19:00:11 -0300

    null e undefined

---
+++ Cod3r/4-fundamentos/nullUndefined.js
@@ -0,0 +1,11 @@
+let value  // nao inicializada
+console.log(value)
+
+value = null // ausencia de valor
+console.log(value)
+
+const produto = {}
+console.log(produto.preco)
+
+produto.preco = 3.12
+console.log(produto)
