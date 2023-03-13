b7b743f99c5ba91a9f7a668c3647fb0353c78bfb
Débora <deboraparecidamas@gmail.com>
2023-03-13 18:53:58 -0300

    objeto

---
+++ Cod3r/4-fundamentos/objeto.js
@@ -0,0 +1,13 @@
+const produto = {}
+produto.nome = 'Celular'
+produto.preco = 166.59
+
+const outroproduto = {
+    nome: 'Camiseta',
+    preco: 10.00,
+    etiqueta: {
+        material: 'poliester',
+        tamanho: 'M'
+    }
+}
+
